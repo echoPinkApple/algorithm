b300d5d213d69e91ea30f90b9da174d46f8939fe
xin <wangxingming@hxct.com>
2023-03-09 08:44:41 +0800

    算法

--- src/main/java/org/echo/arithmetic/ArithmeticApplication.java
+++ src/main/java/org/echo/arithmetic/ArithmeticApplication.java
@@ -2,12 +2,25 @@ package org.echo.arithmetic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ArithmeticApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ArithmeticApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(ArithmeticApplication.class, args);
+        for (String name : run.getBeanDefinitionNames()) {
+            System.out.println(name);
+        }
+
+//        AnnotationConfigApplicationContext applicationContext=new AnnotationConfigApplicationContext(BeanTest.class );
+//        for (String name : applicationContext.getBeanDefinitionNames()) {
+//            System.out.println(name);
+//        }
     }
 
 }
---
+++ src/main/java/org/echo/arithmetic/BeanTest.java
@@ -0,0 +1,23 @@
+package org.echo.arithmetic;
+
+import lombok.Setter;
+import org.echo.arithmetic.domain.ListNode;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+/**
+ * @projectName: algorithm
+ * @package: org.echo.arithmetic
+ * @className: BeanTest
+ * @author: xin
+ * @description: TODO
+ * @date: 2023/3/8 11:00
+ * @version: 1.0
+ */
+@Service
+public class BeanTest {
+    @Bean
+    public ListNode listNode(){
+        return new ListNode(10);
+    }
+}
